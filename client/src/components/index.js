9cd881dd7dc13fd0eaf2b05ec9ef1394941644bf
pipgainer <53998714+pipgainer@users.noreply.github.com>
2023-09-02 03:50:44 +0530

    first commit
    added by dragging files

---
+++ client/src/components/index.js
@@ -0,0 +1,14 @@
+import CustomButton from "./CustomButton";
+import ColorPicker from "./ColorPicker";
+import AIPicker from "./AIPicker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
+
+
+export { 
+    CustomButton,
+    ColorPicker,
+    AIPicker,
+    FilePicker,
+    Tab
+};
